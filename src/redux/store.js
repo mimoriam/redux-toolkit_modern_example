31fc44345913668990ebb3c4f1e78e651af22c38
Mimo <mimoriam420@gmail.com>
2022-11-26 21:08:38 +0500

    Commit #7 - Commented a way to add multiple reducers in the main store

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,6 +3,9 @@ import counterReducer from './counterSlice';
 
 export const store = configureStore({
     reducer: {
-        counter: counterReducer
+        counter: counterReducer,
+        // To register another reducer in the store, we could do:
+        // users: usersReducer,
+        // posts: postsReducer
     },
 })
