656cfd9dbbd8d0199e6691c4c415d013ca6f21e0
Mimo <mimoriam420@gmail.com>
2022-11-26 20:52:57 +0500

    Commit #4 - Created a Slice and provided it to our store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit'
+import counterReducer from './counterSlice';
 
 export const store = configureStore({
-    reducer: {},
+    reducer: {
+        counter: counterReducer
+    },
 })
