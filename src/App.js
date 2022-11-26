c2fdc6663e7ccebbe49dce6dc2c34e7ed441b83d
Mimo <mimoriam420@gmail.com>
2022-11-26 21:01:28 +0500

    Commit #5 - Used Redux in components (useSelector & useDispatch)

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
-function App() {
-  return (
-   <>
-     <h1>App works!</h1>
-   </>
-  );
-}
+import { useSelector, useDispatch } from 'react-redux';
+import { increment, decrement, incrementByAmount } from './redux/counterSlice';
+
+const App = () => {
+
+    const count = useSelector((state) => state.counter.value)
+    const dispatch = useDispatch();
+
+    return (
+        <>
+            {count} <br/>
+
+            <button onClick={() => dispatch(increment())}>Increment</button>
+
+            <button onClick={() => dispatch(decrement())}>Decrement</button>
+
+            <button onClick={() => dispatch(incrementByAmount(Number(2) || 0))}>Increment</button>
+        </>
+    )
+};
 
 export default App;
